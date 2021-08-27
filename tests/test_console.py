0664d16f5113e3f147f5d2272aeb5b49a0c43fd9
radifar <m.radifar05@gmail.com>
2021-08-27 16:51:25 +0700

    add mock test

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,27 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+@pytest.fixture
+def mock_request_get(mocker):
+    mock = mocker.patch("requests.get")
+    mock.return_value.__enter__.return_value.json.return_value = {
+        "title": "Lorem Ipsum",
+        "extract": "Lorem ipsum dolor sit amet",
+    }
+    return mock
+
+
+def test_main_succeeds(runner, mock_request_get):
     result = runner.invoke(console.main)
     assert result.exit_code == 0
+
+
+def test_main_prints_title(runner, mock_request_get):
+    result = runner.invoke(console.main)
+    assert "Lorem Ipsum" in result.output
+
+
+def test_main_uses_en_wikipedia_org(runner, mock_request_get):
+    runner.invoke(console.main)
+    args, _ = mock_request_get.call_args
+    assert "en.wikipedia.org" in args[0]
